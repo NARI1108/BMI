b0742592c3d147a199da0c870ce2cfb65dd6b663
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-16 16:00:33 +0330

    Writing commands that will direct us to that activity by clicking on any of the buttons. Or it will take us out.

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        btn_scale_bmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,bmi_Activity.class));
+            }
+        });
+        btn_about_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,about_Us_Activity.class));
+            }
+        });
+        btn_contact_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(new Intent(MainActivity.this,contact_Us_Activity.class));
+            }
+        });
+    }
+    public void btn_click(View view){
+          switch(view.getId()){
+              case R.id.btn_exit:finish();
+          }
     }
     public void findViews(){
         btn_scale_bmi = findViewById(R.id.btn_scale_bmi);
