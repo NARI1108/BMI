5fd02fb456de8b615ca80c1f6b1ffcc5939a32c7
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-16 15:33:42 +0330

    Creating a method and then by means of that we connect all the perspectives from the XML layer to this activity.

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_scale_bmi, btn_about_us, btn_contact_us, btn_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
+    public void findViews(){
+        btn_scale_bmi = findViewById(R.id.btn_scale_bmi);
+        btn_about_us  = findViewById(R.id.btn_about_us);
+        btn_contact_us= findViewById(R.id.btn_contact_us);
+        btn_exit      = findViewById(R.id.btn_exit);
+    }
+
 }
