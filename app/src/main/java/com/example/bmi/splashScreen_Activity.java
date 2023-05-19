e19e7aa4dbb72397789424c3cb10d365146d6bcb
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-19 13:17:42 +0330

    Sorting the codes of this activity for more readability.

--- app/src/main/java/com/example/bmi/splashScreen_Activity.java
+++ app/src/main/java/com/example/bmi/splashScreen_Activity.java
@@ -10,23 +10,31 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class splashScreen_Activity extends AppCompatActivity {
-
+    ImageView img_splashScreen;
+    Animation animation_img_splashScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-          ImageView img_splashScreen = findViewById(R.id.img_splashScreen);
-
-          Animation  animation_img_splashScreen = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
-
-          img_splashScreen.setAnimation(animation_img_splashScreen);
-          new Handler().postDelayed(new Runnable() {
-              @Override
-              public void run() {
-                  startActivity(new Intent(splashScreen_Activity.this,MainActivity.class));
-                  finish();
-              }
-          },4000);
+          findViews();
+          animation();
+          splashScreen();
+    }
+    public void findViews(){
+        img_splashScreen = findViewById(R.id.img_splashScreen);
+    }
+    public void animation(){
+        animation_img_splashScreen = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
+        img_splashScreen.setAnimation(animation_img_splashScreen);
+    }
+    public void splashScreen(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashScreen_Activity.this,MainActivity.class));
+                finish();
+            }
+        },4000);
     }
 }
