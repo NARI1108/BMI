d599fa68550a4bf9b1dad4dda26f1705480a0e56
Hill <125409221+NARI1108@users.noreply.github.com>
2023-05-23 16:07:21 +0330

    Creating a bmiApp method for bmi_Activity.java that To calculate your weight and tell you how much your weight is too low or too much and tell you what type of obesity you are overweight, we have classified the level of overweight into three levels.

--- app/src/main/java/com/example/bmi/bmi_Activity.java
+++ app/src/main/java/com/example/bmi/bmi_Activity.java
@@ -3,18 +3,24 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class bmi_Activity extends AppCompatActivity {
     TextView txt_name, txt_weight, txt_tall, txt_gender, txt_result;
     EditText edt_name, edt_weight, edt_tall;
     RadioButton rdo_mr, rdo_miss;
     Button  btn_scale;
+    int Weight, tall_centimeter, Weight_Suitable_0, Weight_Suitable_1, Weight_Suitable_2;
+    String weight, tall, name;
+    float tall_meter, result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,7 @@ public class bmi_Activity extends AppCompatActivity {
 
         findViews();
         animation();
+        bmiApp();
     }
     public void findViews(){
         txt_name = findViewById(R.id.txt_name);
@@ -60,5 +67,52 @@ public class bmi_Activity extends AppCompatActivity {
         rdo_mr.setAnimation(animation_rdo_mr);
         rdo_miss.setAnimation(animation_rdo_miss);
         btn_scale.setAnimation(animation_btn_scale);
+    }
+    public void bmiApp(){
+        btn_scale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 name = edt_name.getText().toString().trim();
+                 weight = edt_weight.getText().toString().trim();
+                 tall = edt_tall.getText().toString().trim();
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(weight) || TextUtils.isEmpty(tall)){
+                    Toast.makeText(getApplicationContext(), "All items must be filled", Toast.LENGTH_SHORT).show();
+                }else{
+
+                    Weight =Integer.parseInt(edt_weight.getText().toString());
+                    tall_centimeter =Integer.parseInt(edt_tall.getText().toString());
+
+                    tall_meter =(float)tall_centimeter/100;
+
+                    tall_meter =(float)Math.pow(tall_meter,2);
+
+                    result = Weight/tall_meter;
+
+                     Weight_Suitable_0 = (int)(24 * tall_meter);
+                     Weight_Suitable_1 =  Weight - Weight_Suitable_0;
+                     Weight_Suitable_2 =  Weight_Suitable_0 - Weight;
+
+                     if(rdo_miss.isChecked()){
+                         result--;
+                     }
+                     
+                     if(result<16.5){
+                         txt_result.setText(name+"Dear, you are severely underweight"+Weight_Suitable_2+"You are underweight.");
+                     } else if (result < 18.40 && result >= 16) {
+                         txt_result.setText(name+"Dear you, approx"+Weight_Suitable_2+"You are underweight.");
+                     } else if (result < 25 && result >= 18.40) {
+                         txt_result.setText(name+"Dear, your weight and height are in proportion, but it can be better");
+                     } else if (result < 30 && result >= 25) {
+                         txt_result.setText(name+"Dear you, approx"+Weight_Suitable_0+"Are you overweight");
+                     }else if  (result < 35 && result >= 30 ){
+                         txt_result.setText(name+"Dear, you have type 1 obesity and approx"+Weight_Suitable_0+"Are you overweight");
+                     } else if (result < 40 && result >= 35 ) {
+                         txt_result.setText(name+"Dear, you have type 2 obesity and approx"+Weight_Suitable_0+"Are you overweight");
+                     }else if(result > 40){
+                         txt_result.setText(name+"Dear, you have type 3 obesity and approx"+Weight_Suitable_0+"take care of yourself");
+                     }
+                }
+            }
+        });
     }
 }
